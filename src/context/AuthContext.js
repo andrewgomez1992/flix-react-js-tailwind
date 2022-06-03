afa7ea68507b091e4d314a174143cbc877145ea6
Andrew Gomez <drewgomez209@gmail.com>
2022-06-03 12:03:28 -0700

    firebase connected

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -18,8 +18,32 @@ import {
 const AuthContext = createContext()
 
 export function AuthContextProvider({ children }) {
+
+    const [user, setUser] = useState({})
+
+    function signUp(email, password) {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    function logIn(email, password) {
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    function logOut() {
+        return signOut(auth)
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser)
+        })
+        return () => {
+            unsubscribe()
+        }
+    }, [])
+
     return (
-        <AuthContext.Provider>
+        <AuthContext.Provider value={{ signUp, logIn, logOut, user }}>
             {children}
         </AuthContext.Provider>
     )
