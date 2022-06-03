00c69a3205f5a5853758ae804755a50f0c6817ef
Andrew Gomez <drewgomez209@gmail.com>
2022-06-02 21:42:27 -0700

    play and watch later buttons added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -26,6 +26,10 @@ const Main = () => {
             <div className='w-full h-full'>
                 <div className='absolute w-full h-[550px] bg-gradient-to-r from-black'></div>
                 <img className='w-full h-full object-cover' src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
+                <div>
+                    <button className='border bg-gray-300 text-black border-gray-300 py-2 px-5'>Play</button>
+                    <button className='border text-white border-gray-300 py-2 px-5 ml-4'>Watch Later</button>
+                </div>
             </div>
         </div>
     )
