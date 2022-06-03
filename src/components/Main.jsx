87254ff2fbd55228d1ddb94de9a577a209eba4fb
Andrew Gomez <drewgomez209@gmail.com>
2022-06-02 21:30:07 -0700

    movie image showing

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -24,7 +24,7 @@ const Main = () => {
     return (
         <div className='w-full h-[550px] text-white'>
             <div className='w-full h-full'>
-                <img src="" alt={movie?.title} />
+                <img src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
             </div>
         </div>
     )
